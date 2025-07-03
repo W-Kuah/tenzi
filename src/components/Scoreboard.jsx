40f27120dfaffa5b0958a24b0d616bc1852bef1e
W-Kuah <45501552+W-Kuah@users.noreply.github.com>
2025-07-03 20:52:16 +1000

    flex-wrap scores

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,12 +1,27 @@
+import one from '../assets/medals/1.png';
+import two from '../assets/medals/2.png';
+import three from '../assets/medals/3.png';
+import four from '../assets/medals/4.png';
+import five from '../assets/medals/5.png';
+
+
 export default function Scoreboard(props) {
   const {numOfDice, topScores} = props;
   const scores = topScores[numOfDice];
+
+  const picIdx = {
+    0: one,
+    1: two,
+    2: three,
+    3: four,
+    4: five,
+  }
   const ScoresElems = () => {
     return (
         <div className="scores">
             {scores.map((score, idx) => (
                 <div className="scoreGroup" key={idx}>
-                    <img className="scoreIcon" src={`src/assets/medals/${idx+1}.png`} alt={`${idx+1}`} />
+                    <img className="scoreIcon" src={picIdx[idx]} alt={`${idx+1}`} />
                     <span>{score}</span>
                 </div>
             ))}
